8bc374a8d690cf91a32b1a9895d7bbc60930286e
Darshan Acharya <acharyadarshan02@gmail.com>
2023-12-08 09:22:01 -0800

    Added intro

--- src/App.js
+++ src/App.js
@@ -22,6 +22,47 @@ function App() {
             <div className="header">
                 <h1>UTXO VISUALIZATION</h1>
             </div>
+            <div class="info-container">
+                <header className="sub-heading">
+                    <h1>BITCOIN UTXO STATS</h1>
+                </header>
+                <section class="description">
+                    <p>
+                        The Heatmap visualizes the 11,000+ unspent transaction outputs in
+                        Resilient db. Each row and column label represents the public keys
+                        of 50 users participating in the transaction. Each cell represents
+                        the number of unspent transaction outputs between two users. The
+                        intensity of color of the cell indicates the quantity of unspent
+                        transaction outputs between them.
+                    </p>
+                </section>
+                <section class="stats">
+                    <div class="stat">
+                        <h2>11,000</h2>
+                        <p>TOTAL TRANSACTIONS</p>
+                    </div>
+                    <div class="stat">
+                        <h2>534</h2>
+                        <p>MAX UNSPENT OUTPUTS IN A CELL</p>
+                    </div>
+                    <div class="stat">
+                        <h2>34,562</h2>
+                        <p>TOTAL UNSPENT IN ALL CELLS</p>
+                    </div>
+                    <div class="stat">
+                        <h2>632</h2>
+                        <p>MAX TRANSACTIONS IN A SINGLE DAY</p>
+                    </div>
+                    <div class="stat">
+                        <h2>14</h2>
+                        <p>MOST ACTIVE USER TRANSACTIONS</p>
+                    </div>
+                    <div class="stat">
+                        <h2>50</h2>
+                        <p>TOTAL USERS</p>
+                    </div>
+                </section>
+            </div>
             <YearSlider minYear={2018} maxYear={2023} onYearChange={handleYearChange} />
             <Heatmap transactionCount={transactionCount} currentYear={currentYear} />
         </div>
